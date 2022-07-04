df3d8f4f235f8ba81b2b48f04955ce837d6be7cf
julio-silveira <julioqueiroz15@gmail.com>
2022-07-04 16:26:27 -0300

    Requesitos 6 e 12 concluídos

--- scripts.js
+++ scripts.js
@@ -13,5 +13,21 @@ function generatePixels(rows, columns) {
     pixelBoard.appendChild(r);
   }
 }
+function generateColors() {
+  let red = Math.random() * 255;
+  let green = Math.random() * 255;
+  let blue = Math.random() * 255;
+  let rgb = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  return rgb;
+}
+
+function selectColors() {
+  colorPallete.children[0].style.backgroundColor = 'Black';
+  colorPallete.children[0].classList.add('selected');
+  for (let index = 1; index < 4; index += 1) {
+    colorPallete.children[index].style.backgroundColor = generateColors();
+  }
+}
 
 generatePixels(5, 5);
+selectColors();
