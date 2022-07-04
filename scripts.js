4902a337caaeb52a9d0eccdacbb0849aac646132
julio-silveira <julioqueiroz15@gmail.com>
2022-07-04 16:44:01 -0300

    Correção de Bugs

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
 const colorPallete = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
+const colorOne = colorPallete.children[0];
+const colorTwo = colorPallete.children[1];
+const colorThree = colorPallete.children[2];
+const colorFour = colorPallete.children[3];
+let selectedColor = colorOne;
 
 function generatePixels(rows, columns) {
   for (let index = 0; index < rows; index += 1) {
-    let r = document.createElement('div');
+    const r = document.createElement('div');
     r.className = 'row';
     for (let index2 = 0; index2 < columns; index2 += 1) {
-      let c = document.createElement('div');
+      const c = document.createElement('div');
       c.className = 'pixel';
       r.appendChild(c);
     }
@@ -14,20 +19,20 @@ function generatePixels(rows, columns) {
   }
 }
 function generateColors() {
-  let red = Math.random() * 255;
-  let green = Math.random() * 255;
-  let blue = Math.random() * 255;
-  let rgb = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  const red = Math.random() * 255;
+  const green = Math.random() * 255;
+  const blue = Math.random() * 255;
+  const rgb = 'rgb(' + red + ',' + green + ',' + blue + ')';
   return rgb;
 }
 
-function selectColors() {
-  colorPallete.children[0].style.backgroundColor = 'Black';
-  colorPallete.children[0].classList.add('selected');
+function setColors() {
+  colorOne.style.backgroundColor = 'Black';
+  selectedColor.classList.add('selected');
   for (let index = 1; index < 4; index += 1) {
     colorPallete.children[index].style.backgroundColor = generateColors();
   }
 }
 
 generatePixels(5, 5);
-selectColors();
+document.onload = setColors();
