4a9968de661689a65a4383e8c42c5a4a3c6e1f5e
julio-silveira <julioqueiroz15@gmail.com>
2022-07-04 19:41:59 -0300

    Requesitos 10 e 11 concluidos

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const colorPallete = document.getElementById('color-palette');
 const pixelBoard = document.getElementById('pixel-board');
 let selectedColor = colorPallete.children[0];
 const clearButton = document.getElementById('clear-board');
+const size = document.getElementById('board-size');
+const changeButton = document.getElementById('generate-board');
 
 function generatePixels(rows, columns) {
   for (let index = 0; index < rows; index += 1) {
@@ -15,6 +17,11 @@ function generatePixels(rows, columns) {
     pixelBoard.appendChild(r);
   }
 }
+
+generatePixels(5, 5);
+let pixelRowsCount = pixelBoard.children.length;
+let pixelColsCount = pixelBoard.children[0].children.length;
+
 function generateColors() {
   const red = Math.random() * 255;
   const green = Math.random() * 255;
@@ -30,6 +37,11 @@ function setColors() {
     colorPallete.children[index].style.backgroundColor = generateColors();
   }
 }
+function getColor() {
+  selectedColor.classList.remove('selected');
+  selectedColor = colorPallete.children[cpindex];
+  selectedColor.classList.add('selected');
+}
 
 function selectColor() {
   for (let cpindex = 0; cpindex < 4; cpindex += 1) {
@@ -39,18 +51,44 @@ function selectColor() {
         selectedColor.classList.remove('selected');
         selectedColor = colorPallete.children[cpindex];
         selectedColor.classList.add('selected');
-        console.log(this);
       }
     );
   }
 }
+function removeRows() {
+  pixelRowsCount = pixelBoard.children.length;
+  pixelColsCount = pixelBoard.children[0].children.length;
 
-generatePixels(5, 5);
+  for (let removeIndex = 0; removeIndex < pixelRowsCount; removeIndex += 1) {
+    pixelBoard.removeChild(pixelBoard.lastChild);
+  }
+}
 
-const pixelRowsCount = pixelBoard.children.length;
-const pixelColsCount = pixelBoard.children[0].children.length;
+setColors();
+
+changeButton.addEventListener('click', function changeSize() {
+  let newSize = parseInt(size.value);
+  if (newSize === NaN) {
+    alert('Board inválido!');
+  } else if (newSize <= 5) {
+    removeRows();
+    generatePixels(5, 5);
+    paintPixel();
+  } else if (newSize >= 50) {
+    removeRows();
+    generatePixels(50, 50);
+    paintPixel();
+  } else if (newSize > 5 && newSize < 50) {
+    removeRows();
+    generatePixels(newSize, newSize);
+    paintPixel();
+  } else alert('Board inválido!');
+});
 
 function paintPixel() {
+  pixelRowsCount = pixelBoard.children.length;
+  pixelColsCount = pixelBoard.children[0].children.length;
+
   for (let crindex = 0; crindex < pixelRowsCount; crindex += 1) {
     for (let ccindex = 0; ccindex < pixelColsCount; ccindex += 1) {
       let selectedPixel = pixelBoard.children[crindex].children[ccindex];
@@ -58,7 +96,6 @@ function paintPixel() {
       selectedPixel.addEventListener('click', function paint() {
         selectedPixel.style.backgroundColor =
           selectedColor.style.backgroundColor;
-        console.log(this);
       });
     }
   }
@@ -75,7 +112,6 @@ function clearBoard() {
   });
 }
 
-document.onload = setColors();
 selectColor();
 paintPixel();
 clearBoard();
